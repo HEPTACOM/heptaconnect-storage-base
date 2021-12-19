0bdc774422c80753df3fae83e4dcc8aa10306c5f
Joshua Behrens <behrens@heptacom.de>
2021-12-19 11:59:09 +0100

    Remove unused JobAdd class
    CONNECT-291

---
+++ Contract/JobPayloadKeyInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base\Contract;
+
+use Heptacom\HeptaConnect\Portal\Base\StorageKey\Contract\StorageKeyInterface;
+
+/**
+ * @deprecated
+ */
+interface JobPayloadKeyInterface extends StorageKeyInterface
+{
+}
