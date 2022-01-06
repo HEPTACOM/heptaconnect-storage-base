f0a206963dbe0342d5bd33e186497a0f78b1c559
Joshua Behrens <behrens@heptacom.de>
2022-01-06 03:29:53 +0100

    Add HttpClientUtilityFactoryInterface to create HTTP client tooling for portals
    CONNECT-200

---
+++ Contract/Action/Job/Finish/JobFinishActionInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base\Contract\Action\Job\Finish;
+
+interface JobFinishActionInterface
+{
+    public function finish(JobFinishPayload $payload): JobFinishResult;
+}
---
+++ Contract/Action/WebHttpHandlerConfiguration/Find/WebHttpHandlerConfigurationFindActionInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base\Contract\Action\WebHttpHandlerConfiguration\Find;
+
+use Heptacom\HeptaConnect\Storage\Base\Exception\UnsupportedStorageKeyException;
+
+interface WebHttpHandlerConfigurationFindActionInterface
+{
+    /**
+     * @throws UnsupportedStorageKeyException
+     */
+    public function find(WebHttpHandlerConfigurationFindCriteria $criteria): WebHttpHandlerConfigurationFindResult;
+}
