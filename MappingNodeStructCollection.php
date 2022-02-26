66b19ee96dcb275f8e40e8bf50f453fabcb07ab8
Julian Krzefski <krzefski@heptacom.de>
2022-02-26 22:45:00 +0100

    CONNECT-25: Remove obsolete portal-node-key from FileReferenceResolver

---
+++ MappingNodeStructCollection.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heptacom\HeptaConnect\Storage\Base;
+
+use Heptacom\HeptaConnect\Dataset\Base\Support\AbstractObjectCollection;
+use Heptacom\HeptaConnect\Storage\Base\Contract\MappingNodeStructInterface;
+
+/**
+ * @extends AbstractObjectCollection<MappingNodeStructInterface>
+ */
+class MappingNodeStructCollection extends AbstractObjectCollection
+{
+    /**
+     * @psalm-return Contract\MappingNodeStructInterface::class
+     */
+    protected function getT(): string
+    {
+        return MappingNodeStructInterface::class;
+    }
+}
